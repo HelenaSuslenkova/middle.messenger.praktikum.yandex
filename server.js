ccf1effeafe87a336c188be506c079d3fff7a182
Helena Suslenkova <helena.suslenkova@gmail.com>
2023-02-17 16:15:02 +0300

    [sprint_1] minor fixes for nodejs server

--- server.js
+++ server.js
@@ -1,37 +1,10 @@
-const express = require('express')
-const { engine } = require('express-handlebars');
+const express = require('express');
 
-const app = express()
-const port = 3000;
+const app = express();
+const PORT = 3000;
 
-app.engine(
-  'hbs',
-  engine({
-    layoutsDir: __dirname + '/src/layouts',
-    partialsDir: __dirname + '/src/partials',
-    defaultLayout: 'base/base',
-    extname: 'hbs',
-  })
-);
+app.use(express.static(`${__dirname}/dist`));
 
-app.set('view engine', 'hbs');
-
-// app.use(express.static(__dirname + '/dist'));
-
-// app.get('/login', function (request, response) {
-//   response.render(__dirname + '/src/pages/signin/signin.hbs', {
-//     title: 'Login',
-//   });
-// });
-
-// app.get('/registration', function (request, response) {
-//   response.render(__dirname + '/src/pages/registration/registration.hbs', {
-//     title: 'Registration',
-//   });
-// });
-
-app.get('/', function (request, response) {
-  response.render(__dirname + '/dist')
-})
-
-app.listen(port, () => console.log(`App listening to port ${port}`));
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}!`);
+});
